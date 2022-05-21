3b82f01251b5fdca68fa55393820eee62a5a1d87
xJZx <92456053+xJZx@users.noreply.github.com>
2022-05-21 21:58:47 +0200

    Add files via upload

---
+++ FrameProjekt/Frame/src/Unit/Swordsman.java
@@ -0,0 +1,42 @@
+package Unit;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class Swordsman extends Unit {
+    //velocity
+    int vxS, vyS;
+    public Swordsman(){
+        vxS=(int)(Math.random()*(1+1)+-2);
+        vyS=(int)(Math.random()*(1+1)+-2);
+        status=2;
+    }
+    //dla testu
+    public void paintS(Graphics g, ArrayList<Swordsman> Army2){
+        switch(status){
+            case 0:
+                g.setColor(Color.WHITE);
+            case 2:
+                g.setColor(Color.RED);
+        }
+        g.fillOval(x,y,10,10);
+
+        getMoveSwordsman();
+    }
+    public void getMoveSwordsman(){
+        x+=vxS;
+        y+=vyS;
+
+        if(x<0 || x>=790){
+            vxS*=-1;
+        }
+        if(y<0 || y>=590){
+            vyS*=-1;
+        }
+    }
+    /*public static void getMoveKnight(int x, int y){
+        x+=vxK;
+        y+=vyK;
+    }*/
+}
