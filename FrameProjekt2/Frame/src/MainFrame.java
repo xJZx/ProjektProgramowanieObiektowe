696edb5cd514ae189d72af2b6e4594cb7a71543c
xJZx <92456053+xJZx@users.noreply.github.com>
2022-05-27 18:33:31 +0200

    Add files via upload

---
+++ FrameProjekt2/Frame/src/MainFrame.java
@@ -0,0 +1,84 @@
+import javax.swing.*;
+
+import PackageUnit.Knight;
+import PackageUnit.Pikeman;
+import PackageUnit.Swordsman;
+import PackageUnit.Unit;
+import PackageUnit.PackageTeam.Blue;
+import PackageUnit.PackageTeam.Red;
+import PackageUnit.PackageTeam.Team;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JPanel implements ActionListener {
+    //presety armii (trzeba je zapełnic jakos)
+    /*ArrayList<Knight> Army1 = new ArrayList<Knight>();
+    ArrayList<Swordsman> Army2 = new ArrayList<Swordsman>();
+    ArrayList<Pikeman> Army3 = new ArrayList<Pikeman>();*/
+
+    public static void main(String[] arg){
+        MainFrame f = new MainFrame();
+    }
+
+    Blue blue = new Blue(); //dziwne obiekty, zeby nie bylo static'ow
+    Red red = new Red();
+    Unit unit = new Unit();
+
+    public MainFrame(){ //konstruktor na okno aplikacji
+        JFrame frame = new JFrame("Simulation");
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        blue.teamSelectorBlue();    //metoda wybierajaca preset armii
+        red.teamSelectorRed();
+
+        Timer timer = new Timer(10,this);   //odswiezanie ekranu w ms
+        timer.restart();
+
+        frame.add(this);
+
+        frame.setVisible(true);
+    }
+
+    public void paint(Graphics g){  //metoda od grafiki
+        //g.fillOval(10,10,100,100);
+        super.paintComponent(g);
+        for(Unit u1: blue.Army1){
+            blue.paintBlue(g, blue.Army1);  //metoda przydzielajaca kolor unitom
+            if(u1.getStatus()==1){
+                ((Knight) u1).shapeK(g, blue.Army1);
+            }
+            if(u1.getStatus()==2){
+                ((Swordsman) u1).shapeS(g, blue.Army1);
+            }
+            if(u1.getStatus()==3){
+                ((Pikeman) u1).shapeP(g, blue.Army1);
+            }
+        }
+        for(Unit u2: red.Army2){
+            red.paintRed(g, red.Army2); //metoda przydzielajaca kolor unitom
+            if(u2.getStatus()==1){
+                ((Knight) u2).shapeK(g, red.Army2);
+            }
+            if(u2.getStatus()==2){
+                ((Swordsman) u2).shapeS(g, red.Army2);
+            }
+            if(u2.getStatus()==3){
+                ((Pikeman) u2).shapeP(g, red.Army2);
+            }
+        }
+        for(int i=0; i<blue.Army1.size(); i++) { //przeszukiwanie arrayow za kolizja
+            for(int j=0; j<red.Army2.size(); j++){
+                    unit.collision(blue.Army1.get(i), red.Army2.get(j));
+            }
+        }
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+}
---
+++ FrameProjekt2/Frame/src/PackageUnit/PackageTeam/Blue.java
@@ -0,0 +1,63 @@
+package PackageUnit.PackageTeam;
+
+import java.util.Scanner;
+
+import PackageUnit.Knight;
+import PackageUnit.Pikeman;
+import PackageUnit.Swordsman;
+import PackageUnit.Unit;
+
+import java.util.ArrayList;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Blue extends Team{
+    public ArrayList<Unit> Army1 = new ArrayList<Unit>();
+    Scanner scanner = new Scanner(System.in);
+    public void teamSelectorBlue() {
+        System.out.println("Select team Blue preset: ");
+        int selector = scanner.nextInt();
+        switch(selector){
+            case 1:
+                //wypelnianie tablic jednostkami
+                for(int i=0; i<30; i++){
+                    Army1.add(new Knight());
+                }
+                for(int i=0; i<10; i++){
+                    Army1.add(new Swordsman());
+                    Army1.add(new Pikeman());
+                }
+            case 2:
+                //wypelnianie tablic jednostkami
+                for(int i=0; i<30; i++){
+                    Army1.add(new Swordsman());
+                }
+                for(int i=0; i<10; i++){
+                    Army1.add(new Knight());
+                    Army1.add(new Pikeman());
+                }
+            case 3:
+                //wypelnianie tablic jednostkami
+                for(int i=0; i<30; i++){
+                    Army1.add(new Pikeman());
+                }
+                for(int i=0; i<10; i++){
+                    Army1.add(new Knight());
+                    Army1.add(new Swordsman());
+                }
+        }
+    }
+    //metoda paintBlue / paintRed tak jak w Unitach ale z większą ilościa statusów dla ktorych ta sama farba będzie
+    public void paintBlue(Graphics g, ArrayList<Unit> Army1) {
+        switch(getStatus()){
+            case 0:
+                g.setColor(Color.WHITE);
+            case 1:
+                g.setColor(Color.BLUE);
+            case 2:
+                g.setColor(Color.BLUE);
+            case 3:
+                g.setColor(Color.BLUE);
+        }
+    }
+}
